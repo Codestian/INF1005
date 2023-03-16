9a592e72cdeaf5716c12e2663efa671d58c2349e
Codestian <mh.silvine@gmail.com>
2023-03-17 01:44:41 +0800

    feat: made login page nice

--- routes/Login.php
+++ routes/Login.php
@@ -5,38 +5,23 @@
 <div class="container-fluid">
     <div class="row">
         <div class="col-lg-6 login-container left-login">
-            <div class="login-hero-text">
-                <h2>Food is symbolic of love when words are inadequate.</h2>
-            </div>
-            <div class="login-hero-gradient"></div>
-            <div class="login-hero-overlay"></div>
-            <img src="https://images.unsplash.com/photo-1600891964599-f61ba0e24092?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1920&q=80" alt="">
+            <?php include("views/login/Background.php"); ?>
         </div>
         <div class="col-lg-6 login-container right-login">
-            <form>
-                <img class="mb-4" src="/docs/5.2/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">
-                <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
-
-                <div class="form-floating">
-                    <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-                    <label for="floatingInput">Email address</label>
-                </div>
-                <div class="form-floating">
-                    <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-                    <label for="floatingPassword">Password</label>
-                </div>
-
-                <div class="checkbox mb-3">
-                    <label>
-                        <input type="checkbox" value="remember-me"> Remember me
-                    </label>
-                </div>
-                <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
-                <p class="mt-5 mb-3 text-muted">&copy; 2017–2022</p>
-            </form>
+            <?php include("views/login/Form.php"); ?>
         </div>
     </div>
 </div>
+
+<script>
+    document.getElementById("login-form").addEventListener("submit", function(event) {
+        event.preventDefault();
+        fetch("/api/v1/users")
+            .then(response => response.json())
+            .then(data => console.log(data))
+            .catch(error => console.error(error));
+    });
+</script>
 
 <style>
     body {
@@ -48,11 +33,11 @@
         display: flex;
         justify-content: center;
         align-items: center;
+        position: relative;
     }
 
     .left-login {
         padding: 0;
-        position: relative;
     }
 
     .login-hero-text {
@@ -61,12 +46,6 @@
         bottom: 0;
         left: 0;
         z-index: 3;
-    }
-
-    .login-hero-text h2 {
-        color: white;
-        font-weight: bold;
-        font-size: 2.4rem;
     }
 
     .login-hero-gradient {
@@ -89,7 +68,7 @@
         z-index: 1;
     }
 
-    img {
+    .left-login img {
         width: 100%;
         height: 100%;
         object-fit: cover;
@@ -99,6 +78,21 @@
         z-index: 0;
     }
 
+    .right-login form {
+        width: 300px;
+        z-index: 1;
+    }
+
+    .right-login img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+        position: absolute;
+        top: 0;
+        left: 0;
+        z-index: 0;
+        opacity: 0.05;
+    }
 
 </style>
 <?php $showFooter = false; include("views/template/Bottom.php"); ?>
---
+++ views/login/Background.php
@@ -0,0 +1,8 @@
+<div class="login-hero-text">
+    <h2 class="display-5 fw-bold text-white">
+        Food is symbolic of love when words are inadequate.
+    </h2>
+</div>
+<div class="login-hero-gradient"></div>
+<div class="login-hero-overlay"></div>
+<img src="https://images.unsplash.com/photo-1600891964599-f61ba0e24092?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1920&q=80" alt="">
