22da69709911731826cf7cbb4f019d0c4963fec7
pointlessneedle <manav.manav31@gmail.com>
2023-03-22 21:05:25 +0800

    feat: (incomplete) Added Restaurant Template
    Adding some components I got done for Restaurant Page.

--- views/template/elements/Head.php
+++ views/template/elements/Head.php
@@ -7,11 +7,24 @@
     <link rel="stylesheet" type="text/css" href="../../../public/css/bootstrap.css">
     <link rel="stylesheet" type="text/css" href="../../../public/css/index.css?v=<?php echo time(); ?>">
     <link rel="stylesheet" type="text/css" href="../../../public/css/hover.css?v=<?php echo time(); ?>">
+    <!-- Own Carousel CSS -->
+    <!-- Swiffy Slider CSS -->
+    <link href="own-carousel.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/swiffy-slider@1.6.0/dist/css/swiffy-slider.min.css" rel="stylesheet" crossorigin="anonymous">
     <!-- jQuery Script -->
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <!-- Maphighlight Script -->
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/maphilight/1.4.0/jquery.maphilight.min.js"></script>
     <!-- Custom JS -->
     <script defer src="../../../public/js/main.js"></script>
+    <!-- Popper JS from Bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <!-- Own Carousel JS -->
+    <script src="own-carousel.min.js"></script>
+    <!-- Swiffy Slider JS -->
+    <script src="https://cdn.jsdelivr.net/npm/swiffy-slider@1.6.0/dist/js/swiffy-slider.min.js" crossorigin="anonymous" defer></script>
+
 </head>
 
