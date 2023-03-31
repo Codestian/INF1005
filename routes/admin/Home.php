2e69b7968e9fc6fb53fc5a9bc69c42b8815532d3
Mhidayah19 <mhidbml@gmail.com>
2023-03-31 18:32:59 +0800

    Admin dashboard working

--- routes/admin/Home.php
+++ routes/admin/Home.php
@@ -5,8 +5,11 @@ include("views/template/Top.php"); ?>
     <div class="d-flex">
         <?php include("views/admin/components/Sidebar.php"); ?>
         <section class="container-fluid" style="padding: 0; height: 100vh; overflow: auto;">
-            <h1>Welcome to Admin page</h1>
+            <h1 style="text-align: center;">Welcome to Admin page</h1>
+            <?php include ("views/admin/components/Home.php");?>
         </section>
+
     </div>
+
 
 <?php $showFooter=false; include("views/template/Bottom.php"); ?>
