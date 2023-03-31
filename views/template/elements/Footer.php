e3a8003878bfbd6116dd4e71725fc60d7271b97d
Mhidayah19 <mhidbml@gmail.com>
2023-03-31 19:30:39 +0800

    Added Terms and conditions page and privacy page at the footer

--- views/template/elements/Footer.php
+++ views/template/elements/Footer.php
@@ -1,3 +1,3 @@
 <footer class="d-flex py-5 bg-dark justify-content-center">
-    <p class="text-white"> © 2023 <img src="../../../public/images/piza-smol.png" alt="pizza-icon">Choppy, Inc</p>
+    <p class="text-white"> © 2023 <img src="../../../public/images/piza-smol.png" alt="pizza-icon">Choppy, Inc. <a href="/terms-and-conditions" class="text-white mx-3">Terms and Conditions</a> | <a href="/privacy-policy" class="text-white mx-3">Privacy Policy</a></p>
 </footer>
